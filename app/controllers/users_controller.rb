3e55bc18d0517469ba2e76e6f54fc59c08970416
EC2 Default User <ec2-user@ip-172-31-4-109.ec2.internal>
2022-02-18 05:40:13 +0000

    [Update]レイアウト完成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def index
     @book = Book.new
+    @users = User.all
     @user = User.find(current_user.id)
   end
 
--- app/models/book.rb
+++ app/models/book.rb
@@ -2,5 +2,5 @@ class Book < ApplicationRecord
   has_one_attached :profile_image
   belongs_to :user
   
-  
+
 end
